b7fab1ea32593a9978c3af83c121fb260398de16
Wahid <wahid.carvan@gmail.com>
2020-10-21 10:48:06 -0400

    week 4 assigment

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,37 @@
                 }
             });
         });
+
+
+// WORKING ON TASK 2
+        // id reserveButton
+        // id loginButton
+// start the function after page has loaded
+    // $(function () {
+        //select the button
+    $("#reserveButton").click(function() {
+         //  display the modal
+    $("#reserveModal").modal("show");
+      //select the button
+       });
+     $("#loginButton").click(function(){
+//         //  display the modal
+    $("#loginModal").modal();
+   
+   });
+// });
+   
+  
+//      $(function () {
+//         //select the button
+//      $("#loginButton").click(function(){
+//         //  display the modal
+//     $("#loginModal").modal();
+//     });
+  
+// });
+
+
+
+
+
